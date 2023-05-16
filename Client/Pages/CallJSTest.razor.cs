81395f809575e8eb31581b4d5b135831c275aae4
Wufferman <eaajbst@students.eaaa.dk>
2023-05-16 16:03:51 +0200

    Test start

---
+++ Client/Pages/CallJSTest.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace JSInjectTest.Client.Pages
+{
+    public partial class CallJSTest
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        private IJSObjectReference _jsModule;
+
+        public string myMessage = "This Is My Test Message String";
+
+        protected override async Task OnInitializedAsync()
+        {
+            _jsModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./scripts/jsTest.js");
+        }
+
+        public async Task ShowAlertWindow(string message)
+        {
+            await _jsModule.InvokeVoidAsync("fun");
+        }
+
+        public async Task ShowOldDevices()
+        {
+            await _jsModule.InvokeVoidAsync("showOld");
+        }
+
+    }
+}
---
+++ Client/wwwroot/scripts/jsTest.js
@@ -0,0 +1,43 @@
+﻿export function showAlert(message) {
+    alert(message);
+}
+export async function fun() {
+
+    let device = await navigator.hid.requestDevice({ filters: [{ vendorId: 5426 }] });
+    console.log(device);
+    navigator.hid.addEventListener("disconnect", ({ device }) => {
+        console.log("Oh noes");
+    });
+    navigator.hid.addEventListener("connect", ({ device }) => {
+        console.log("Yay");
+    });
+
+    for (let collection of device[1].collections) {
+        // An HID collection includes usage, usage page, reports, and subcollections.
+        console.log(`Usage: ${collection.usage}`);
+        console.log(`Usage page: ${collection.usagePage}`);
+
+        for (let inputReport of collection.inputReports) {
+            console.log(`Input report: ${inputReport.reportId}`);
+            // Loop through inputReport.items
+        }
+
+        for (let outputReport of collection.outputReports) {
+            console.log(`Output report: ${outputReport.reportId}`);
+            // Loop through outputReport.items
+        }
+
+        for (let featureReport of collection.featureReports) {
+            console.log(`Feature report: ${featureReport.reportId}`);
+            // Loop through featureReport.items
+        }
+
+        // Loop through subcollections with collection.children
+    }
+
+} 
+export async function showOld(){
+    let devices = await navigator.hid.getDevices();
+   
+
+}
